4d8738fb6fdb6eb430abf3d66b1e20fc3bda0a66
colelevy <colelevy08@gmail.com>
2023-03-07 15:15:20 -0500

    commit tree 2004c5cadb7e110e609f2f5dacde79946c977835

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+
+function writeCards(people, event){
+    const arrayOne = [];
+    for (let F = 0; F < people.length; F++){
+   
+    arrayOne.push(`Thank you, ${people[F]}, for the wonderful ${event} gift!`)
+}
+return arrayOne;
+}
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+
+function countDown(num) {
+    while (num >= 0) {
+      console.log(num);
+      num--;
+    }
+  }
+
+  countDown(10);
